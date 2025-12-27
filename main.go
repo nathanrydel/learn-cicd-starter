41e53828571cee9b4f981aeab2a072c2df98ca0b
Nathan Rydel <43457298+nathanrydel@users.noreply.github.com>
2025-12-27 10:36:53 -0600

    refactor: add in unused func for linting fail

--- main.go
+++ main.go
@@ -24,6 +24,11 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
+func unused() {
+	// this func does nothing
+	// and is called nowhere
+}
+
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
